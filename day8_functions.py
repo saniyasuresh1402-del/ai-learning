bc953296f187d699eabb49cef0938ba994390f8b
saniyasuresh1402-del <saniyasuresh1402@gmail.com>
2026-01-18 15:39:21 +0530

    day 8 : python functions

---
+++ day8_functions.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return a + b
+def substract(a, b):
+    return a - b
+def greet(name):
+    return "hello " + name
+print(add(5, 3))
+print(substract(10, 4))
+print(greet("saniya"))
