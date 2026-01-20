52578da23a786bc4c5cae60d95636adbb9f49114
saniyasuresh1402-del <saniyasuresh1402@gmail.com>
2026-01-21 00:12:53 +0530

    day 10:if elif else conditions

---
+++ day10_if_elif_else.py
@@ -0,0 +1,14 @@
+temperature = 35
+if temperature > 30:
+    print("hot weather" )
+elif temprature > 20:
+    print("moderate weather")
+else:
+    print("cold weather")
+voltage = 3.2
+if voltage >= 3.3:
+    print("logic high")
+else:
+    print("logic low")
+
+
